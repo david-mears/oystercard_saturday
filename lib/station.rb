407a59191500dafd59604ef84297c456d1bd935c
david-mears <kieukmail@gmail.com>
2019-01-16 17:11:06 +0000

    Added more tests for Station.new

--- lib/station.rb
+++ lib/station.rb
@@ -2,4 +2,18 @@ class Station
 
   attr_reader :name, :zone
 
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end
+
 end
+
+# File.open("zonelist", 'r').each do |line|
+#   name, zone = line.split(',')
+#   new_station = Station.new(name,)
+#   all_stations = []
+#   all_stations.push(new_station)
+# end
+#
+# puts all_stations
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,4 +5,14 @@ describe Station do
   it { is_expected.to respond_to :name }
   it { is_expected.to respond_to :zone }
 
+  subject {described_class.new("Old Street", 1)}
+
+  it 'knows its name' do
+    expect(subject.name).to eq("Old Street")
+  end
+
+  it 'knows its zone' do
+    expect(subject.zone).to eq(1)
+  end
+
 end
